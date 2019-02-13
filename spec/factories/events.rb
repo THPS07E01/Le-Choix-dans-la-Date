fb481ec27292bbd5e841e1a5cfb569af34f25073
TheWalkingNev <alexandre.gasior@gmail.com>
2019-02-13 20:37:19 +0100

    Maman m'a dit que j'peux !

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :event do
+    admin_id { FactoryBot.create(:user).id }
     start_date { Faker::Date.forward(rand(1..666)) }
-    duration { rand(0..100) * 5 }
+    duration { rand(1..100) * 5 }
     title { Faker::DrWho.catch_phrase }
     description { Faker::DrWho.quote + Faker::HowIMetYourMother.quote }
     price { rand(1..1000) }
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,54 +6,63 @@ RSpec.describe Event, type: :model do
     @event = FactoryBot.build(:event)
   end
 
-  context "validation" do
+  it "has a valid factory" do
+    expect(build(:event)).to be_valid
+  end
 
+  context "validation" do
     it "is valid with valid attributes" do
       expect(@event).to be_a(Event)
     end
-
-    it "is a valid date for start_date" do
+    it "#start_date" do
       expect @event.start_date.class == Date
-    end
-
-    it "checks start_date is not past" do
       expect @event.start_date > Time.now
     end
-
-    it "is an integer for duration" do
+    it "#duration" do
       expect @event.duration.class == Integer
-    end
-
-    it "is a multiple of 5 for duration" do
       expect @event.duration % 5 == 0
+      expect(@event).to allow_value(5).for(:duration)
+      expect(@event).to allow_value(10).for(:duration)
+      expect(@event).to_not allow_value(5.5).for(:duration)
+      expect(@event).to_not allow_value(-5).for(:duration)
+      expect(@event).to_not allow_value(0).for(:duration)
     end
-
-    it "is a string for title" do
+    it "#title" do
       expect @event.title.class == String
-    end
-
-    it "title.length between 5 - 140" do
       expect @event.title.length >= 5 && 140 >= @event.title.length
+      expect(@event).to allow_value('Peepoodo').for(:title)
+      expect(@event).to_not allow_value('Jul').for(:title)  # Refouler Jul direct quand il essaie de rentrer dans la DB, le coquinou pourrait tout nous souiller.
+      expect(@event).to_not allow_value('BG').for(:title) # Déso Félix :p
+      expect(@event).to_not allow_value(nil).for(:title)
     end
-
-    it "is a string for description" do
+    it "#description" do
       expect @event.description.class == String
-    end
-
-    it "description.length to be between 20 - 1000" do
       expect @event.description.length >= 20 && 1000 >= @event.description.length
+      expect(@event).to allow_value("J'aime pas les étrangers").for(:description)
+      expect(@event).to allow_value('12345678912345678912').for(:description) # String.length 20
+      expect(@event).to_not allow_value('1234567891234567891').for(:description)  # String.length 19
+      expect(@event).to_not allow_value(nil).for(:description)
     end
-
-    it "is a string for price" do
+    it "#price" do
       expect @event.price.class == Integer
-    end
-
-    it "price to be between 1 - 1000" do
       expect @event.price >= 1 && 1000 >= @event.price
+      expect(@event).to allow_value(1).for(:price)
+      expect(@event).to allow_value(500).for(:price)
+      expect(@event).to allow_value(1000).for(:price)
+      expect(@event).to_not allow_value(1001).for(:price)
+      expect(@event).to_not allow_value(nil).for(:price)
     end
-
-    it "is a string for location" do
+    it "#location" do
       expect @event.location.class == String
+      expect(@event).to allow_value('Bar le 1969').for(:location)
+      expect(@event).to allow_value('Le grand théatre').for(:location)
+      expect(@event).to_not allow_value(nil).for(:location)
     end
+  end
+
+  context "associations" do
+  	it { expect(@event).to have_many(:attendances) }
+  	it { expect(@event).to have_many(:attendees) }
+  	it { expect(@event).to belong_to(:admin) }
   end
 end
